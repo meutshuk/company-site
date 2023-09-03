89dbb84eb03cf6e8c33d842facdc9f00838c2318
Utshuk Dhamala <me.utshuk1001@outlook.com>
2023-09-03 13:02:48 +1000

    tresting

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Work from "./components/Work";
 import Toggle from "./components/Toggle";
 import Marquee from "react-fast-marquee";
 import Header from "./components/Navbar/Header";
+import MouseHover from "./components/MouseHover/MouseHover";
 
 function App() {
   const [isDay, setIsDay] = useState(true); // default to day mode
@@ -44,23 +45,73 @@ function App() {
   return (
     <>
       {/* Video backgroud */}
-      <Video darkMode={darkMode} />
-
+      {/* <Video darkMode={darkMode} /> */}
       {/* Navbar with overlay when clicked open */}
       {/* <Navbar darkMode={darkMode} toggleDayNight={toggleDayNight} /> */}
       {/* 
       https://blog.olivierlarose.com/demos/text-clip-mask-on-scroll
       */}
+      <section className=" flex justify-center bg-background text-color">
+        <div className="container py-20 border-b-2 border-red">
+          <div>
+            <div className="text-3xl font-medium lowercase">You</div>
+            <div className="flex items-center gap-6">
+              <div>Logo</div>
+              <div className="text-6xl font-bold uppercase">Imagine</div>
+            </div>
+          </div>
+          <div className="flex flex-col w-full items-end">
+            <div className="lowercase text-3xl font-medium">We</div>
+            <div className="font-bold text-6xl uppercase">Create</div>
+          </div>
+        </div>
+      </section>
+      {/* video area */}
+      <section className="flex mt-10 justify-center bg-background text-color">
+        <div className="container pb-20 border-b-2 border-red">
+          <div className="flex justify-between items-center capitalize text-sm pb-10">
+            <div className="">From imagination to animation</div>
+            <div>Tell us what you want and we will make it happen</div>
+          </div>
+
+          <div className="bg-red text-center h-[50vh]  rounded-3xl overflow-hidden">
+            Video
+          </div>
+        </div>
+      </section>
+
+      <section className="flex justify-center bg-background text-color mt-20 ">
+        <div className="container">
+          <div className="text-base">VR</div>
+          <div className="py-7 px-5 w-full md:w-[55%]  text-3xl polka">
+            Step into a world where your story unfolds not on screens, but all
+            around you. Leveraging the cutting-edge capabilities of VR paired
+            with compelling animation, our team is dedicated to transporting
+            audiences into narratives that resonate, inspire, and leave an
+            indelible mark.
+          </div>
+        </div>
+      </section>
+
+      {/* <section className="mt-20 p-5 flex justify-center bg-color text-background rounded-3xl">
+        <div className="container">
+
+        </div>
+      </section> */}
+
+      <section className="flex justify-center my-20 bg-color text-background rounded-[3rem] py-20 ">
+        <div className="container">
+          <div className="font-medium text-lg my-5">ANIMATION</div>
+          <MouseHover />
+        </div>
+      </section>
 
       {/* New Navbar bubble effect */}
       <Header />
-
       {/* Toggle Button */}
       <Toggle darkMode={darkMode} toggleDayNight={toggleDayNight} />
-
       {/* Main */}
       <Main />
-
       {/* Marquess with texts */}
       <section className=" ">
         <Marquee
@@ -90,10 +141,8 @@ function App() {
           </div>
         </Marquee>
       </section>
-
       {/* Animations */}
       <Work />
-
       {/* Footer */}
       <Footer />
     </>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,51 +9,76 @@ import {
 
 export default function Footer() {
   return (
-    <footer className="bg-ascent text-white py-10">
-      <div className="container mx-auto px-6 lg:px-16">
-        <div className="flex flex-wrap justify-between">
-          <div className="w-full md:w-1/2 mb-6 md:mb-0">
-            <h4 className="text-xl font-bold mb-4 text-color">Contact</h4>
-            <div className="flex items-center mb-4">
-              <FaEnvelope className="mr-4 text-lg text-red " />
-              <span className="text-color">
-                Email:{" "}
-                <a
-                  href="mailto:support@example.com"
-                  className="text-red hover:underline"
-                >
-                  utshuk@utshuk.com
-                </a>
-              </span>
+    // <footer className="bg-ascent text-white py-10">
+    //   <div className="container mx-auto px-6 lg:px-16">
+    //     <div className="flex flex-wrap justify-between">
+    //       <div className="w-full md:w-1/2 mb-6 md:mb-0">
+    //         <h4 className="text-xl font-bold mb-4 text-color">Contact</h4>
+    //         <div className="flex items-center mb-4">
+    //           <FaEnvelope className="mr-4 text-lg text-red " />
+    //           <span className="text-color">
+    //             Email:{" "}
+    //             <a
+    //               href="mailto:support@example.com"
+    //               className="text-red hover:underline"
+    //             >
+    //               utshuk@utshuk.com
+    //             </a>
+    //           </span>
+    //         </div>
+    //         <div className="flex items-center">
+    //           <FaPhone className="mr-4 text-lg text-red" />
+    //           <span className="text-color">
+    //             Phone:{" "}
+    //             <a href="tel:+11234567890" className="text-red hover:underline">
+    //               041231231
+    //             </a>
+    //           </span>
+    //         </div>
+    //       </div>
+    //       <div className="w-full md:w-1/2">
+    //         <h4 className="text-xl font-bold mb-4 text-color">Follow Us</h4>
+    //         <div className="flex space-x-4">
+    //           <a href="#" className="text-red hover:text-blue-400">
+    //             <FaFacebookF className="text-2xl " />
+    //           </a>
+    //           <a href="#" className="text-red hover:text-blue-400">
+    //             <FaTwitter className="text-2xl" />
+    //           </a>
+    //           <a href="#" className="text-red hover:text-blue-400">
+    //             <FaInstagram className="text-2xl" />
+    //           </a>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div className="mt-10 border-t pt-6 flex justify-center border-gray-700">
+    //       <p className="text-sm text-red">© Utshuk.</p>
+    //     </div>
+    //   </div>
+    // </footer>
+    <footer className="flex justify-center bg-red py-6">
+      <div className="container ">
+        <div className="mx-auto flex justify-between items-center">
+          <div className="text-white font-bold">Logo</div>
+
+          <div className="flex space-x-6">
+            <div className="space-y-2">
+              <div className="text-white cursor-pointer">Home</div>
+              <div className="text-white cursor-pointer">About</div>
+              <div className="text-white cursor-pointer">Contact</div>
             </div>
-            <div className="flex items-center">
-              <FaPhone className="mr-4 text-lg text-red" />
-              <span className="text-color">
-                Phone:{" "}
-                <a href="tel:+11234567890" className="text-red hover:underline">
-                  041231231
-                </a>
-              </span>
+            <div className="space-y-2">
+              <div className="text-white cursor-pointer">Facebook</div>
+              <div className="text-white cursor-pointer">Insta</div>
+              <div className="text-white cursor-pointer">Twitter</div>
             </div>
-          </div>
-          <div className="w-full md:w-1/2">
-            <h4 className="text-xl font-bold mb-4 text-color">Follow Us</h4>
-            <div className="flex space-x-4">
-              <a href="#" className="text-red hover:text-blue-400">
-                <FaFacebookF className="text-2xl " />
-              </a>
-              <a href="#" className="text-red hover:text-blue-400">
-                <FaTwitter className="text-2xl" />
-              </a>
-              <a href="#" className="text-red hover:text-blue-400">
-                <FaInstagram className="text-2xl" />
-              </a>
+            <div className="space-y-2">
+              <div className="text-white">Email</div>
+              <div className="text-white">Phone number</div>
             </div>
           </div>
         </div>
-        <div className="mt-10 border-t pt-6 flex justify-center border-gray-700">
-          <p className="text-sm text-red">© Utshuk.</p>
-        </div>
+        <div className="text-center">@utshuk</div>
       </div>
     </footer>
   );
